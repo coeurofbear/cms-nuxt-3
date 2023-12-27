095f478ba522fb53b473cd1daf843c7d05970709
Gíssela Reyes <coeurofbear@gmail.com>
2023-12-27 20:18:10 +0100

    feat: make pages from Storyblok dynamic

---
+++ pages/[...slug].vue
@@ -0,0 +1,13 @@
+<script setup>
+const { slug } = useRoute().params
+ 
+const story = await useAsyncStoryblok(
+  slug && slug.length > 0 ? slug.join('/') : 'home',
+  { version: 'draft' }
+).catch(() => {throw createError({statusCode: 404,statusMessage: 'Page Not Found',fatal: true});})
+
+</script>
+ 
+<template>
+  <StoryblokComponent v-if="story" :blok="story.content" />
+</template>
