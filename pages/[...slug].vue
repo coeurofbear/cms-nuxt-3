b4f6e266c125dd9c5d7d81f2f9fe1f7bc6b3b5bc
Gíssela Reyes <coeurofbear@gmail.com>
2023-12-28 12:25:04 +0100

    feat: handle error page

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,11 +1,16 @@
 <script setup>
-const { slug } = useRoute().params
- 
-const story = await useAsyncStoryblok(
-  slug && slug.length > 0 ? slug.join('/') : 'home',
-  { version: 'draft' }
-).catch(() => {throw createError({statusCode: 404,statusMessage: 'Page Not Found',fatal: true});})
+const { slug } = useRoute().params;
 
+const story = await useAsyncStoryblok(
+  slug && slug.length > 0 ? slug.join("/") : "home",
+  { version: "draft" }
+).catch(() => {
+  throw createError({
+    statusCode: 404,
+    statusMessage: "Page Not Found",
+    fatal: true,
+  });
+});
 </script>
  
 <template>
