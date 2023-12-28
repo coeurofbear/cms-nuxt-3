46c93223b98bbfe51c3c3390d8005f9263ddcbe0
Gíssela Reyes <coeurofbear@gmail.com>
2023-12-28 12:25:13 +0100

    feat: use richtext

--- storyblok/Teaser.vue
+++ storyblok/Teaser.vue
@@ -1,12 +1,13 @@
 <template>
-  <div
-    v-editable="blok"
-    class="py-32 text-6xl text-[#50b0ae] font-bold text-center"
-  >
-    {{ blok.headline }}
+  <div v-editable="blok" class="text-center">
+    <h1 class="mt-32 mb-4 text-6xl text-[#50b0ae] font-bold">
+      {{ blok.headline }}
+    </h1>
+    <div v-html="resolvedRichText"/>
   </div>
 </template>
  
 <script setup>
-defineProps({ blok: Object })
+const props = defineProps({ blok: Object })
+const resolvedRichText = computed(() => renderRichText(props.blok.text))
 </script>
