be9eb7cb012280e8ca5752e0967ffa45d83a8979
Gíssela Reyes <coeurofbear@gmail.com>
2023-12-28 14:57:20 +0100

    feat: create blog

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,13 +1,20 @@
 <script setup>
 const { slug } = useRoute().params;
 const { locale } = useI18n();
+const resolveRelations = ['popular-articles.articles']
 const story = await useAsyncStoryblok(
-  slug && slug.length > 0 ? slug.join("/") : "home",
-  { version: "draft" }
+  slug && slug.length > 0 ? slug.join('/') : 'home',
+  {
+    version: 'draft',
+    resolve_relations: resolveRelations,
+  },
+  {
+    resolveRelations,
+  }
 ).catch(() => {
   throw createError({
     statusCode: 404,
-    statusMessage: "Page Not Found",
+    statusMessage: 'Page Not Found',
     fatal: true,
   });
 });
