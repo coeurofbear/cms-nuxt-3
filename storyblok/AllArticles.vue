c428e6df4669acd9c59c92c9d09d44f85335a334
Gíssela Reyes <coeurofbear@gmail.com>
2023-12-30 01:17:08 +0100

    feat: rename all articles

--- storyblok/AllArticles.vue
+++ storyblok/AllArticles.vue
@@ -3,7 +3,7 @@
     <h2 class="text-6xl text-[#50b0ae] font-bold text-center mb-12">{{ blok.headline }}</h2>
     <div class="container mx-auto grid md:grid-cols-3 gap-12 my-12 place-items-start">
       <ArticleCard
-        v-for="article in blok.articles"
+        v-for="article in articles"
         :key="article.uuid"
         :article="article.content"
         :slug="article.full_slug"
@@ -14,4 +14,15 @@
  
 <script setup>
 defineProps({ blok: Object })
+ 
+const { locale } = useI18n()
+const storyblokApi = useStoryblokApi()
+ 
+const { data } = await storyblokApi.get('cdn/stories', {
+  version: 'draft',
+  language: locale.value,
+  starts_with: 'blog',
+  is_startpage: false,
+})
+const articles = ref(data.stories)
 </script>
